25db9454a916e2dc2da1df977dc206f3d65e7bbf
Nghia251506 <ntn8530@gmail.com>
2025-02-06 11:43:07 +0700

    fix product and productdetail

--- App/Models/Product_Detail.model.js
+++ App/Models/Product_Detail.model.js
@@ -3,20 +3,14 @@ const conn = require('../common/connect');
 ProductDetail = function(productdetail){
     this.id = productdetail.id;
     this.product_id = productdetail.product_id;
-    this.label = productdetail.label;
-    this.item_1 = productdetail.item_1;
-    this.item_2 = productdetail.item_2;
-    this.item_3 = productdetail.item_3;
-    this.item_4 = productdetail.item_4;
-    this.item_5 = productdetail.item_5;
-    this.item_6 = productdetail.item_6;
-    this.item_7 = productdetail.item_7;
-    this.item_8 = productdetail.item_8;
+    this.attribute_id = productdetail.attribute_id;
+    this.val = productdetail.val;
+    this.supplier_id = productdetail.supplier_id;
 }
 
 
 ProductDetail.getProductDetailById = function(id, result){
-    conn.query(`SELECT * FROM product_details WHERE product_id = ${id}`, function(err, productdetail){
+    conn.query(`SELECT pd.*, at.*, sp.*  FROM product_details as pd INNER JOIN attributes as at ON at.id = pd.attribute_id INNER JOIN suppliers as sp ON sp.id = pd.supllier_id WHERE product_id = ${id}`, function(err, productdetail){
         if(err){
             result(null, err);
         }else{
@@ -26,7 +20,7 @@ ProductDetail.getProductDetailById = function(id, result){
 }
 
 ProductDetail.createProductDetail = function(productdetail, result){
-    conn.query('INSERT INTO product_details(product_id, label, item_1,item_2,item_3,item_4,item_5,item_6,item_7,item_8) VALUES (?,?,?,?,?,?,?,?,?,?)', productdetail, function(err, res){
+    conn.query('INSERT INTO product_details(product_id, attribute_id, val, supplier_id) VALUES (?,?,?,?,?)', productdetail, function(err, res){
         if(err){
             result(err, null);
         }else{
@@ -36,7 +30,7 @@ ProductDetail.createProductDetail = function(productdetail, result){
 }
 
 ProductDetail.updateProductDetail = function(id, productdetail, result){
-    conn.query('UPDATE product_details SET label=?, item_1=?, item_2=?, item_3=?, item_4=?, item_5=?, item_6=?, item_7=?, item_8=? WHERE product_id=?', [productdetail.label, productdetail.item_1, productdetail.item_2, productdetail.item_3, productdetail.item_4, productdetail.item_5, productdetail.item_6, productdetail.item_7, productdetail.item_8, id], function(err, res){
+    conn.query('UPDATE product_details SET attribute_id = ?, val = ?, supplier_id = ? WHERE product_id=?', [productdetail.attribute_id, productdetail.val, productdetail.supplier_id, id], function(err, res){
         if(err){
             result(null, err);
         }else{
